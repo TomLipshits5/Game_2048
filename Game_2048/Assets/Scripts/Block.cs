412b7b6b451e0b071c99356261588679de5b2df5
TomCooll <tom20101995@gmail.com>
2022-03-02 16:20:44 +0200

    finished grid

--- Game_2048/Assets/Scripts/Block.cs
+++ Game_2048/Assets/Scripts/Block.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Block : MonoBehaviour
 {
     private int value;
     [SerializeField] private SpriteRenderer renderer;
+    [SerializeField] private TextMeshPro blockText;
 
 
 
    public void init(BlockType type){
         value = type.value;
         renderer.color = type.color;
+        blockText.text = type.value.ToString();
 
     }
 }
