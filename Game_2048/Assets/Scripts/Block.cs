bc21fa4c5bfe8033d4c7ffbdbe07ba5ef27f3ef5
TomCooll <tom20101995@gmail.com>
2022-03-06 18:37:19 +0200

    almost complite need to fix spawn

--- Game_2048/Assets/Scripts/Block.cs
+++ Game_2048/Assets/Scripts/Block.cs
@@ -9,13 +9,50 @@ public class Block : MonoBehaviour
     private int value;
     [SerializeField] private SpriteRenderer renderer;
     [SerializeField] private TextMeshPro blockText;
+    public Block mergingBlock;
+    public bool isMerging;
+    public Vector2 Pos => transform.position;
+    public Node node;
+
+
+    
 
 
 
-   public void init(BlockType type){
+
+    public void init(BlockType type){
         value = type.value;
         renderer.color = type.color;
         blockText.text = type.value.ToString();
 
     }
+
+
+    public void SetNode(Node node) 
+    {
+        if (this.node != null) this.node.currentBlock = null;
+
+        this.node = node;
+        this.node.currentBlock = this;
+    
+    }
+
+    public int getVal()
+    {
+        return value; 
+    }
+
+
+    public void merge(Block blockToMergeWith)
+    {
+        mergingBlock = blockToMergeWith;
+        mergingBlock.isMerging = true;
+
+        node.currentBlock = null;
+
+
+    }
+
+
+    public bool CanMerge(int value) => value == this.value && mergingBlock == null && !isMerging;
 }
--- Game_2048/Assets/Scripts/GameManager.cs
+++ Game_2048/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using DG.Tweening;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 
@@ -15,13 +17,21 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Node nodePrefab;
     [SerializeField] private SpriteRenderer boardPrefab;
     [SerializeField] private Block blockPrifab;
+    [SerializeField] private float swipeMinimumDistance;
+    [SerializeField] private float travelTime = 0.2f;
+    [SerializeField] private int winCon = 2048;
 
     [SerializeField] public List<BlockType> types = new List<BlockType>();
 
+    [SerializeField] private GameObject winScreen, loseScreen;
 
+    private Vector3 firstPosition;
+    private Vector3 lastPosition;
+    private Swipe swipe;
     private List<Node> nodes = new List<Node>();
     private List<Block> blocks= new List<Block>();
     private GameState gameState;
+    private bool isFirstTurn = true;
 
 
 
@@ -31,14 +41,21 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        generateGrid();
+        swipeMinimumDistance = Screen.height * 15 / 100;
+        changeGameState(GameState.GenerateLevel);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState != GameState.WaitForInput) return;
+
+        swipe = getPlayerInput();
+        changeGameState(GameState.Move);
+        changeGameState(GameState.WaitForInput);
+
+
     }
 
 
@@ -47,14 +64,138 @@ public class GameManager : MonoBehaviour
     private void changeGameState(GameState state){
         gameState = state;
 
-        switch (state)
-        {
+        switch(gameState){
+            case GameState.GenerateLevel:
+                generateGrid();
+                break;
+            case GameState.SpawnBlocks:
+                getNewBlock(isFirstTurn ? 2: 1);
+                isFirstTurn = false;
+                break;
+            case GameState.Move:
+                moveBoard(swipe);
+                break;
+            case   GameState.WaitForInput:
+                break;
+            case GameState.Loss:
+                loseScreen.SetActive(true);
+                break;
+            case GameState.Win:
+                winScreen.SetActive(true);
+                break;
             
-            default:
+                default: throw new System.ArithmeticException();
+
+          
         }
-                    
+        
+
+
     }
 
+    private void moveBoard(Swipe swipe)
+    {
+        switch (swipe)
+        {
+            case Swipe.up:
+                shift(Vector2.up);
+                break;
+
+            case Swipe.down:
+                shift(Vector2.down);
+                break;
+
+            case Swipe.right:
+                shift(Vector2.right);
+                break;
+
+            case Swipe.left:
+                shift(Vector2.left);
+                break;
+
+
+            default: 
+                changeGameState(GameState.WaitForInput);
+                break;
+
+        }
+    }
+
+
+    private void shift(Vector2 dir) 
+    {
+        var orderedBlocks =  blocks.OrderBy(b => b.Pos.x).ThenBy(b => b.Pos.y).ToList();
+        if (dir == Vector2.right || dir == Vector2.up) orderedBlocks.Reverse();
+
+        foreach(var block in orderedBlocks)
+        {
+            var next = block.node;
+            do
+            {
+                block.SetNode(next);
+
+                var possibleNode = getNodeAtPos(next.pos + dir);
+                if(possibleNode != null)
+                {
+                    if(possibleNode.currentBlock != null && possibleNode.currentBlock.CanMerge(block.getVal()))
+                    {
+                       block.merge(possibleNode.currentBlock);
+                    }
+
+                    else if(possibleNode.currentBlock == null)
+                    {
+                        next = possibleNode;
+                    }
+                }
+
+            }while (next != block.node);
+
+            
+        }
+
+        var sequnce = DOTween.Sequence();
+
+        foreach(var block in orderedBlocks)
+        {
+            var movePoint = block.mergingBlock != null ? block.mergingBlock.node.pos : block.node.pos;
+
+            sequnce.Insert(0, block.transform.DOMove(movePoint, travelTime));
+
+        }
+
+        sequnce.OnComplete(() =>
+        {
+            foreach (var block in blocks.Where(b => b.mergingBlock != null))
+            {
+                mergeBlocks(block.mergingBlock, block);
+            }
+            changeGameState(GameState.SpawnBlocks);
+        });
+
+    }
+
+
+    private void mergeBlocks(Block baseBlock, Block other)
+    {
+        spawnBlock(baseBlock.node,getBlockType(baseBlock.getVal() * 2));
+        removeBlock(baseBlock);
+        removeBlock(other);
+
+
+    }
+
+
+    private void removeBlock(Block block)
+    {
+        blocks.Remove(block);
+        Destroy(block.gameObject);
+    }
+
+    private Node getNodeAtPos(Vector2 pos)
+    {
+        Node node = nodes.FirstOrDefault(n => n.pos == pos);
+        return node;
+    }
 
     void generateGrid(){
         
@@ -72,23 +213,110 @@ public class GameManager : MonoBehaviour
 
         Camera.main.transform.position = new Vector3(center.x,center.y,-10);
 
-        getNewBlock(2);
+        changeGameState(GameState.SpawnBlocks);
     }
 
 
 
-    void getNewBlock(int amount){
+    private void getNewBlock(int amount){
 
         var freeNodes = nodes.Where(n => n.currentBlock == null).OrderBy(b=>Random.value).ToList();
         BlockType blockType = getBlockType(Random.value>0.8 ? 4 : 2);
         foreach (var node in freeNodes.Take(amount))
         {
-            var block = Instantiate(blockPrifab,node.pos,Quaternion.identity);
-            block.init(blockType);
-            blocks.Add(block);
+            spawnBlock(node, blockType);
         }
+
+
+
+        if(freeNodes.Count() == 1)
+        {
+            changeGameState(GameState.Loss);
+            return;
+        }
+
+        changeGameState(blocks.Any(b => b.getVal() == winCon) ? GameState.Win : GameState.WaitForInput);
         
     }
+
+
+    private void spawnBlock(Node node,BlockType blockType)
+    {
+        var block = Instantiate(blockPrifab, node.pos, Quaternion.identity);
+        block.init(blockType);
+        block.SetNode(node);
+        blocks.Add(block);
+    }
+    
+    
+
+    
+    private Swipe getPlayerInput()
+    {
+        Swipe swipe = Swipe.tap;
+        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0); 
+            if(touch.phase == TouchPhase.Began)
+            {
+                firstPosition = touch.position;
+                lastPosition = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Moved)
+            {
+                lastPosition = touch.position;
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                lastPosition = touch.position;
+                float xDis = Mathf.Abs(firstPosition.x - lastPosition.x);
+                float yDis = Mathf.Abs(firstPosition.y - lastPosition.y);
+                swipe = GetSwipeDirection(xDis, yDis);
+               
+            }
+        }
+
+
+
+
+        return swipe;
+    }
+
+
+    private Swipe GetSwipeDirection(float xDis, float yDis)
+    {
+        if (xDis > swipeMinimumDistance || yDis > swipeMinimumDistance)
+        {
+            if (xDis > yDis)
+            {
+                if (lastPosition.x > firstPosition.x)
+                {
+                    return Swipe.right;
+                }
+                else
+                {
+                    return Swipe.left;
+                }
+            }
+            else
+            {
+                if (lastPosition.y > firstPosition.y)
+                {
+                    return Swipe.up;
+                }
+                else
+                {
+                    return Swipe.down;
+                }
+            }
+        }
+        return Swipe.tap;
+    }
+
+
+
+
 }
 
 [System.Serializable]
@@ -108,4 +336,14 @@ public enum GameState{
     WaitForInput,
     Win,
     Loss
+}
+
+
+public enum Swipe
+{
+    right,
+    left,
+    up,
+    down,
+    tap
 }
